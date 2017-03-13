22e416556e52cce27b33c89a2d712ddc9f77dd0e
Sen2017 <casintahan47at@gmail.com>
2017-03-13 08:44:48 -1000

    targetted elements

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ var tagElement = document.getElementsByTagName("script");
 console.log(tagElement);
 
 console.log(document.getElementsByClassName("use-class-method"));
+var classElements = document.getElementsByClassName("use-class-method");	
+
+//changing html elements
+
+console.log(idElement.innerHTML);
+
+idElement.innerHTML = "i have been changed"; //this changed the second element based on its id
+
+console.log(classElements[1]);
