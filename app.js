baedc739a56749437723f460ab21a6acf80be7bf
Sen2017 <casintahan47at@gmail.com>
2017-03-13 08:32:26 -1000

    targetted elements

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('sanity check');
+//finding html elements
+
+//console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method");
+console.log(idElement);
+
+//console.log(document.getElementsByTagName("script"));
+var tagElement = document.getElementsByTagName("script");
+console.log(tagElement);
+
+console.log(document.getElementsByClassName("use-class-method"));
